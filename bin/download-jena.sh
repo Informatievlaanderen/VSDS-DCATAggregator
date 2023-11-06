0868ab58ce9bd2a068b80ed304872d4846f503d3
Sander Van Dooren <sander@essentialcomplexity.eu>
2023-11-06 12:02:34 +0100

    Update version regex.

--- bin/download-jena.sh
+++ bin/download-jena.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-VERSION=`curl https://dlcdn.apache.org/jena/binaries/ 2>/dev/null| sed -n "s/^.*<a href=\"apache-jena-\([[:digit:]].[[:digit:]].[[:digit:]]\).zip\">.*$/\1/p"`
+VERSION=`curl https://dlcdn.apache.org/jena/binaries/ 2>/dev/null| sed -n "s/^.*<a href=\"apache-jena-\([[:digit:]]*.[[:digit:]]*.[[:digit:]]*\).zip\">.*$/\1/p"`
 rm -rf apache-jena apache-jena-*
 wget https://dlcdn.apache.org/jena/binaries/apache-jena-${VERSION}.zip -O apache-jena.zip
 unzip apache-jena.zip
