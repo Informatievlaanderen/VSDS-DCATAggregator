ccbee2c8fa3c4a690755e1e0c699ec2be73530c6
Sander Van Dooren <sander@essentialcomplexity.eu>
2024-01-08 11:42:54 +0100

    Disble ssl checks for geomobility.

--- bin/download.sh
+++ bin/download.sh
@@ -4,4 +4,4 @@ filename=${filename##*/}
 catalog_url=$(echo "$filename" | perl -pe 's/\%(\w\w)/chr hex $1/ge')
 path="workspace/dcat/$filename/original"
 #curl -L -s -o "$path" -z "$path" "$catalog_url"
-curl -L -s -o "$path" "$catalog_url"
+curl --insecure -L -s -o "$path" "$catalog_url"
